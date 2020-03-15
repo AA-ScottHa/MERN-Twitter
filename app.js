9ebca53e47b07d5fee9f6df6b31d61d0b882100e
Scott Ha <123viet123@gmail.com>
2020-03-14 17:56:00 -0700

    Added custom routes

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const db = require("./config/key.js").mongoURI;
+const users = require("./routes/api/users.js");
+const tweets = require("./routes/api/tweets.js");
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB."))
+  .catch(err => console.log(err));
+
 
 app.get("/", (req, res) => {
 	res.send("Hello aA!");
 });
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
 
